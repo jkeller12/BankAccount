b41684b9dca1a853434f768d6997ef15dd8d38e6
Jonah Keller <jkeller10@stuy.edu>
2020-10-28 17:47:10 -0400

    I think I set up everything properly but I don't have a tester file yet and have yet to compile the code so we'll see

--- BankAccount.java
+++ BankAccount.java
@@ -1,24 +1,75 @@
 public class BankAccount
 {
 //Instance variables
-private final double balance;
+private double balance;
 private final int accountID;
-private final String password;
+private String password;
 
 //Constructor
 
-public BankAccount()
+public BankAccount(int ID, String Pass)
 {
   balance = 0;
+  accountID = ID;
+  password = Pass;
 
+  //password = password_set;
 }
 
+
+// getmethods
+
+public double getBalance()
+{
+  return balance;
+}
+
+public int getAccountID()
+{
+  return password;
+}
 //Instance methods
-public double potentialAt()
+
+public String getPassword()
+{
+  return password;
+}
+
+public void setPassword(String newPass)
+{
+  password = newPass;
+}
+
+public boolean deposit(double amount)
+{
+  if (amount >= 0.0)
+  {
+    balance += amount;
+    return true;
+  }
+  return false;
+}
+
+public boolean withdraw(double amount)
+{
+  if (amount >= 0.0 && <= balance)
+  {
+    balance -= amount;
+    return true;
+  }
+  return false;
+}
+
+public String toString()
+{
+  return Strin.valueOf(accountID) +"\t" + "$" +String.valueOf(balance);
+}
+
+
+
+
+public static void main(String[] args)
 {
 
 }
-
-
-
 }
