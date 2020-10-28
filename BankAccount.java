7f74603be06e321e2b909c9d84081ee2ac90ef67
Jonah Keller <jkeller10@stuy.edu>
2020-10-28 16:54:18 -0400

    "I'm very confused"

---
+++ BankAccount.java
@@ -0,0 +1,24 @@
+public class BankAccount
+{
+//Instance variables
+private final double balance;
+private final int accountID;
+private final String password;
+
+//Constructor
+
+public BankAccount()
+{
+  balance = 0;
+
+}
+
+//Instance methods
+public double potentialAt()
+{
+
+}
+
+
+
+}
